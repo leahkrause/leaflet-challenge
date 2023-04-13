5e06fbebc77cf8d8e0e05fe6cb4f4b9f8bb207b8
leahkrause <leahrkrause@gmail.com>
2023-04-12 20:07:12 -0700

    updates

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,8 @@
 
+// Define url 
 var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson';
 
-
+// Define map
 var myMap = L.map("map", {
     center: [37.7749, -122.4194],
     zoom: 4
@@ -12,23 +13,33 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// Creating marker size function
-function markerSize(feature, latlng) {
-  radius =  Math.sqrt(feature.properties.mag) * 5;
+// Creating a color function
+function chooseColor(x) {
+  if (x > 100) return "red";
+  else return "pink";
 }
+
+ // Creating marker size and color function
+function styleInfo (feature, latlng) {
+  return {
+  radius: feature.properties.mag * 5,
+  fillColor: chooseColor(feature.geometry.coordinates[2]),
+  color: chooseColor(feature.geometry.coordinates[2]),
+  opacity: 0.8,
+  fillOpacity: 0.5,
+  };
+};
 
 d3.json(url).then(function(response) {
 
   L.geoJson(response, {
     // Turn each feature into a marker 
     pointToLayer: function (feature, latlng) {
-      return L.circleMarker(latlng, {
-        radius: markerSize(feature.properties.mag)
-      });
+      return L.circleMarker(latlng);
     },
-    // We set the style for each circleMarker using our styleInfo function.
-    // style: styleInfo,
-    // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
+    // Set style 
+    style: styleInfo,
+    // Create popup
     onEachFeature: function (feature, layer) {
       layer.bindPopup(
         "Magnitude: "
@@ -39,92 +50,9 @@ d3.json(url).then(function(response) {
         + feature.properties.place
       );
     }
+  // legend.addTo(myMap);
+
   }).addTo(myMap);
 
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// d3.json(url).then(function(data) {
-  
-//   function styleInfo(feature){
-//     return{
-//       radius: feature.properties.magnitude*4
-//     }
-//   };
-  
-//   L.geoJson(data, {
-//     pointToLayer: function(feature, latlng){
-//       return L.circleMarker(latlng)
-//     }
-//   })
-// }).addTo(myMap);
-
-// // Create a new marker cluster group.
-// var markers = L.markerClusterGroup();
-
-// Loop through the data.
-// for (var i = 0; i < response.length; i++) {
-
-// //     // Set the data location property to a variable.
-// var location = response.features.geometry;
-
-// //     // Check for the location property.
-// if (location) {
-
-// //     // Add a new marker to the cluster group, and bind a popup.
-// markers.addLayer(L.marker([location.coordinates[0], location.coordinates[1]]).bindPopup(response[i].descriptor));
-//  }
-
-// }});
-
-//   // Add our marker cluster layer to the map.
-// myMap.addLayer(markers);
-
-// var features = response['features'];
-  
-// for (var i = 0; i < features.length; i++) {
-//   var location = features.geometry[i];
-  
-//   if (location) {
-//     L.marker([location.coordinates[0], location.coordinates[1]]).addTo(myMap);
-//   }
-// }
-  
-// });
-
-
-
-  ////
-
-    // // Create a new marker cluster group.
-    // var markers = L.markerClusterGroup();
-
-    // // Loop through the data.
-    // for (var i = 0; i < response.length; i++) {
-  
-    //   // Set the data location property to a variable.
-    //   var location = response[i].location;
-  
-    //   // Check for the location property.
-    //   if (location) {
-  
-    //     // Add a new marker to the cluster group, and bind a popup.
-    //     markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-    //       .bindPopup(response[i].descriptor));
-    //   }
-  
-    
